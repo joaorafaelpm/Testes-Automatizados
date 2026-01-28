afd1cf5d0d3c5e2a9e2947d9cd86479f8f0aecbc
João Rafael Pendezza Medeiros <joaorafaelpmed@gmail.com>
2026-01-28 20:12:42 -0300

    Refatorando o código e populando a classe

--- cypress/e2e/step_definitions/login/login.js
+++ cypress/e2e/step_definitions/login/login.js
@@ -3,9 +3,7 @@ import LoginPage from "./loginPage";
 
 const TEST_USERNAME = Cypress.env("TEST_USERNAME");
 const TEST_PASSWORD = Cypress.env("TEST_PASSWORD");
-const TEST_CLIENT_ID = Cypress.env("TEST_CLIENT_ID");
-const TEST_CLIENT_SECRET = Cypress.env("TEST_CLIENT_SECRET");
-const CODE_VERIFIER = Cypress.env("CODE_VERIFIER");
+
 
 Given("I'm on the login page", () => {
   LoginPage.visitLoginPage();
@@ -34,30 +32,5 @@ Then("I should see a error page", () => {
 });
 
 Then("I have a successful login and see my access_token", () => {
-  cy.url().then((url) => {
-    const urlObj = new URL(url);
-    const capturedCode = urlObj.searchParams.get("code");
-
-    expect(capturedCode).to.not.be.null;
-
-    cy.request({
-      method: "POST",
-      url: "http://localhost:80/oauth2/token",
-      form: true,
-      auth: {
-        username: TEST_CLIENT_ID,
-        password: TEST_CLIENT_SECRET,
-      },
-      body: {
-        grant_type: "authorization_code",
-        code: capturedCode,
-        redirect_uri: "http://localhost:80/redirect",
-        code_verifier: CODE_VERIFIER,
-      },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body).to.have.property("access_token");
-      cy.log("Access Token obtido: " + response.body.access_token);
-    });
-  });
+  LoginPage.makeAccessTokenRequest();
 });
--- cypress/e2e/step_definitions/login/loginPage.js
+++ cypress/e2e/step_definitions/login/loginPage.js
@@ -4,6 +4,9 @@ const PASSWORD_INPUT = "#Password";
 const SUBMIT_BUTTON = "#LoginButton";
 const ERROR_MESSAGE_CLASS = ".alert-danger";
 const ERROR_MESSAGE_TEXT = "O e-mail ou senha não confere";
+const TEST_CLIENT_ID = Cypress.env("TEST_CLIENT_ID");
+const TEST_CLIENT_SECRET = Cypress.env("TEST_CLIENT_SECRET");
+const CODE_VERIFIER = Cypress.env("CODE_VERIFIER");
 
 class LoginPage {
   static visitLoginPage() {
@@ -24,6 +27,35 @@ class LoginPage {
 
   static getErrorMessage() {
     cy.get(ERROR_MESSAGE_CLASS).contains(ERROR_MESSAGE_TEXT);
+  }
+
+  static makeAccessTokenRequest() {
+    cy.location("search").then((search) => {
+      const urlParams = new URLSearchParams(search);
+      const capturedCode = urlParams.get("code");
+      cy.log("O código capturado é: " + capturedCode);
+      expect(capturedCode).to.not.be.null;
+      cy.request({
+        method: "POST",
+        url: "http://localhost:80/oauth2/token",
+        form: true,
+        auth: {
+          username: TEST_CLIENT_ID,
+          password: TEST_CLIENT_SECRET,
+        },
+        body: {
+          grant_type: "authorization_code",
+          code: capturedCode,
+          redirect_uri: "http://localhost:80/redirect",
+          code_verifier: CODE_VERIFIER,
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property("access_token");
+        cy.log("Access Token obtido: " + response.body.access_token);
+      });
+    });
+    
   }
 }
 
